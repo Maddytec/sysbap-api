852666df92924f744788983850dac8c837f40657
Madson Silva <maddytec@gmail.com>
2018-11-10 18:37:47 -0200

    Added the class of configuration Swagger

--- src/main/java/br/com/maddytec/SysbapApplication.java
+++ src/main/java/br/com/maddytec/SysbapApplication.java
@@ -3,9 +3,6 @@ package br.com.maddytec;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-@EnableSwagger2
 @SpringBootApplication
 public class SysbapApplication {
 
